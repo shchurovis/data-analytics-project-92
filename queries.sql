a0f8547bf5ec56e84ae028e6d49898f7b0bb9a6e
shchurovis <schurov91@gmail.com>
2026-07-15 00:08:29 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -17,6 +17,43 @@ where s.quantity is not NULL
 group by seller
 order by income desc;
 
+/*отчет по продавцам, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам*/
+select
+    e.first_name || ' ' || e.last_name as seller,
+    FLOOR(AVG(s.quantity * p.price)) as average_income
+from employees as e
+left join sales as s
+    on e.employee_id = s.sales_person_id
+left join products as p
+    on s.product_id = p.product_id
+where s.quantity is not NULL
+group by seller
+order by average_income asc;
+
+/*отчет, содержащий информацию о выручке по дням недели*/
+with table1 as (
+    select
+        e.first_name || ' ' || e.last_name as seller,
+        TO_CHAR(s.sale_date, TRIM('Day')) as day_of_week,
+        FLOOR(SUM(s.quantity * p.price)) as income,
+        EXTRACT(isodow from s.sale_date) as day_number
+    from employees as e
+    inner join sales as s
+        on e.employee_id = s.sales_person_id
+    inner join products as p
+        on s.product_id = p.product_id
+    where s.quantity is not NULL
+    group by seller, day_of_week, day_number
+)
+
+select
+    seller,
+    day_of_week,
+    income
+from table1
+order by day_number asc;
+
+
 
 
 
